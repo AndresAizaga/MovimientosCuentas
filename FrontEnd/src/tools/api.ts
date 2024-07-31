9e29103606a876e89b92e1b936f363cd2933cfc5
Andres Aizaga <serviciosprofesionales.2@registrocivil.gob.ec>
2024-07-31 00:38:00 -0500

    Added frontend project

---
+++ FrontEnd/src/tools/api.ts
@@ -0,0 +1,45 @@
+import { Endpoints } from '../types/endpoints.ts';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import qs from 'qs';
+
+export enum AxiosMethod {
+    Delete = 'Delete',
+    Get = 'Get',
+    Post = 'Post',
+    Put = 'Put',
+}
+
+export interface ApiError {
+    details?:Record<string, unknown>;
+    message: string;
+    name: string;
+    status: number;
+}
+
+export interface ActionResult<T> {
+    data?: T;
+    error?: Error;
+}
+
+export const requestApi = async <T, D>(data: T, endpoint: Endpoints, method: AxiosMethod, param?: string): Promise<D> => {
+  const apiURL = import.meta.env.VITE_BACKEND_URL;
+
+  let url = `${apiURL}${endpoint}`;
+  if (param) url += `${param}`;
+
+  const config: AxiosRequestConfig<D> = { timeout: 30000 };
+
+  let response: AxiosResponse<D, T>;
+
+  if (method === AxiosMethod.Post) {
+    response = await axios.post<D>(url, qs.stringify(data), config);
+  } else if (method === AxiosMethod.Put) {
+    response = await axios.put<D>(url, qs.stringify(data), config);
+  } else if (method === AxiosMethod.Delete) {
+    response = await axios.delete<D>(url, config);
+  } else {
+    response = await axios.get<D>(url, config);
+  }
+
+  return response?.data;
+}
--- MicroServices/MicroClientes/Program.cs
+++ MicroServices/MicroClientes/Program.cs
@@ -25,7 +25,7 @@ var app = builder.Build();
 app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
